8afb874cab5a981fac10090e1065b5e9bdff99c2
Sanyira Pacheco <psanyira@gmail.com>
2026-09-07 19:56:06 -0300

    Add files via upload

---
+++ ejercicio6.js
@@ -0,0 +1,12 @@
+function mostrarPrecio(){
+let selectPizza= document.getElementById("pizza");
+let inputPrecio= document.getElementById("precio");
+let precioSeleccionado= selectPizza.value; 
+if (precioSeleccionado!== ""){
+    inputPrecio.value= "$" + precioSeleccionado;
+}else{
+    inputPrecio.value= "";
+}
+
+}
+
